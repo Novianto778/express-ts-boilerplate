47556577e96459e3f08de7d47eb8493e39db897b
novianto778 <63406837+Novianto778@users.noreply.github.com>
2024-12-29 11:41:10 +0700

    fix: validateRequest  manipulate the request before send to controller

--- src/common/utils/httpHandlers.ts
+++ src/common/utils/httpHandlers.ts
@@ -1,17 +1,22 @@
+import type { ServiceResponse } from "@/common/models/serviceResponse";
 import type { NextFunction, Request, Response } from "express";
 import type { ZodSchema } from "zod";
-
-import type { ServiceResponse } from "@/common/models/serviceResponse";
 import { parseQueryOrParams } from "./queryParams";
 
 export const handleServiceResponse = (serviceResponse: ServiceResponse<any>, response: Response) => {
   return response.status(serviceResponse.statusCode).send(serviceResponse);
 };
 
-export const validateRequest = (schema: ZodSchema) => (req: Request, _res: Response, next: NextFunction) => {
+export const validateRequest = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
   const query = parseQueryOrParams(req.query);
   const params = parseQueryOrParams(req.params);
 
-  schema.parse({ body: req.body, query, params });
+  const result = schema.parse({ body: req.body, query, params });
+
+  // manipulate the request object to include the validated data
+  req.body = result.body;
+  req.query = result.query;
+  req.params = result.params;
+
   next();
 };
